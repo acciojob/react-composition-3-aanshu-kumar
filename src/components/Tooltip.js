96f8844af69ad6b259f85ae6bad6e1435758293a
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2025-01-27 15:28:09 +0530

    Add files via upload

---
+++ src/components/Tooltip.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "../styles/App.css";
+
+const Tooltip = ({ text, children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const showTooltip = () => setIsVisible(true);
+  const hideTooltip = () => setIsVisible(false);
+
+  return (
+    <div
+      className="tooltip"
+      onMouseEnter={showTooltip}
+      onMouseLeave={hideTooltip}
+    >
+      {children}
+      {isVisible && <div className="tooltiptext">{text}</div>}
+    </div>
+  );
+};
+
+export default Tooltip;
